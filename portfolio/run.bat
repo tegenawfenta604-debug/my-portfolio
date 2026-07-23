09f0283458376a7e891594700feb6fb0cbe45063
Tegenaw Fenta <tegenawfenta604@gmail.com>
2026-07-23 04:08:11 +0300

    Update requirements.txt for Render deployment

---
+++ portfolio/run.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\pc\OneDrive\Documents\Desktop\my portifolio\portfolio"
+"C:\Users\pc\AppData\Local\Programs\Python\Python314\python.exe" manage.py runserver
---
+++ portfolio/run.ps1
@@ -0,0 +1,3 @@
+$venvPy = 'C:\Users\pc\OneDrive\Documents\Desktop\my portifolio\portfolio\.venv\Scripts\python.exe'
+Set-Location 'C:\Users\pc\OneDrive\Documents\Desktop\my portifolio\portfolio'
+& $venvPy manage.py runserver
